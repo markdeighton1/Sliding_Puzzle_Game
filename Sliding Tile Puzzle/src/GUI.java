625862250c90bd3f756205b7d5e896f9ae00ae98
Mark <Exile@10.34.113.89>
2018-03-20 23:28:47 +0000

    setup for an array of images and adding it too the JPanel

--- Sliding Tile Puzzle/src/GUI.java
+++ Sliding Tile Puzzle/src/GUI.java
@@ -1,6 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.Event.*;
+
 public class GUI {
 
+	ImageIcon[] bartArray = new ImageIcon[12];
+	
+	public GUI()
+	{
+		
+		JFrame aFrame = new JFrame();
+		JPanel aPanel =  new JPanel(new FlowLayout(FlowLayout.LEFT));
+		
+		aPanel.add(bartArray[0]);
+		
+	}
+	
 }
