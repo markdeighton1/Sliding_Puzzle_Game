0c7ff136ef1811fc6caf448284e3ef8e2ddf652b
Mark <Exile@10.34.113.89>
2018-03-21 01:30:56 +0000

    place the images over the buttons

--- Sliding Tile Puzzle/src/GUI.java
+++ Sliding Tile Puzzle/src/GUI.java
@@ -4,15 +4,21 @@ import java.awt.Event.*;
 
 public class GUI {
 
-	ImageIcon[] bartArray = new ImageIcon[12];
+	ImageIcon[] bartIArray = new ImageIcon[12];
+	JButton[] bartBArray = new JButton[12];
 	
 	public GUI()
 	{
 		
+		
 		JFrame aFrame = new JFrame();
 		JPanel aPanel =  new JPanel(new FlowLayout(FlowLayout.LEFT));
 		
-		aPanel.add(bartArray[0]);
+		bartIArray[0] = new ImageIcon("/bart.jpg");
+		bartBArray[0] = new JButton(bartIArray[0]);
+		
+		
+		aPanel.add(bartBArray[0]);
 		
 	}
 	
