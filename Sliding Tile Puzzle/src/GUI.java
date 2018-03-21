0004eee37f613522b8501fa5df414b942ffd9e8e
Exile <Exile@10.34.113.89>
2018-03-21 02:33:18 +0000

    load each image into array

--- Sliding Tile Puzzle/src/GUI.java
+++ Sliding Tile Puzzle/src/GUI.java
@@ -8,15 +8,22 @@ public class GUI
 	ImageIcon[] bartIArray = new ImageIcon[12];
 	JButton[] bartBArray = new JButton[12];
 	
+	
 	public GUI()
 	{
-		
 		
 		JFrame aFrame = new JFrame();
 		JPanel aPanel =  new JPanel(new FlowLayout(FlowLayout.LEFT));
 		
-		bartIArray[0] = new ImageIcon("bin/bart0.jpg");
-		bartBArray[0] = new JButton(bartIArray[0]);
+		for(int i = 0; i < 12; i ++)
+		{
+			bartIArray[i] = new ImageIcon("bin/bart" + i +  ".jpg");
+			bartBArray[i] = new JButton(bartIArray[i]);
+		}
+		
+		
+		//bartIArray[0] = new ImageIcon("bin/bart0.jpg");
+		//bartBArray[0] = new JButton(bartIArray[0]);
 		
 		
 		aPanel.add(bartBArray[0]);
