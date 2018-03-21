3b7fca0ab10ac7fbfe6e0b7ee3e6e4c0ba0b55b7
Exile <Exile@10.34.113.89>
2018-03-21 03:10:52 +0000

    setup the swap function class and refered to GUI object

--- Sliding Tile Puzzle/src/Swap.java
+++ Sliding Tile Puzzle/src/Swap.java
@@ -1,4 +1,13 @@
 import java.awt.event.*;
-public class Swap {
 
+public class Swap implements ActionListener 
+{
+	GUI Swap; // Declare variable to refer to object
+	
+	public void actionPerformed(ActionEvent actionEvent) 
+	{
+		
+		
+		
+	}
 }
